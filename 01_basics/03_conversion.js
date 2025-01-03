8b3b8fe183c157ca719151a646fe2f9d2017412a
ganpati123 <94175736+ganpati123@users.noreply.github.com>
2025-01-03 06:48:57 +0000

    operations

--- 01_basics/03_conversion.js
+++ 01_basics/03_conversion.js
@@ -1,9 +1,36 @@
 let score = "33abc"
 
-console.log( typeof score);
-console.log(typeof (score));
+// console.log( typeof score);
+// console.log(typeof (score));
 
 let valueInNumber = Number(score);
 
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// operations
+
+let str1 = "hello"
+let str2 = " hitesh"
+
+let str3 = str1 + str2
+// console.log(str3)
+
+// console.log(1 + "2")
+// console.log("1" + 2)
+// console.log("1" + 2 + 2)
+// console.log(1 + 2 + "2")
+// console.log(1 + "2" + 2)
+
+console.log(true)
+
+let gameCounter = 100
+// console.log(gameCounter)
+// gameCounter++;
+//  console.log(gameCounter)
+
+console.log(gameCounter);
+
+++gameCounter;
+console.log(gameCounter)
+console.log(gameCounter);
