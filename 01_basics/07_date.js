163a36682774f1128b9485c01d4a239569ece22f
ganpati123 <94175736+ganpati123@users.noreply.github.com>
2025-01-03 13:20:21 +0000

    l

---
+++ 01_basics/07_date.js
@@ -0,0 +1,18 @@
+// Dates
+
+let myDate = new Date()
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate)
+
+let myCreatedDate = new Date("01-14-2024");
+console.log(myCreatedDate.toDateString())
+
+let myTimeStamp = Date.now()
+console.log(myTimeStamp);
+
+
+
+
